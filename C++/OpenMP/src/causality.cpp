6a2379365491aabfbc8b64bf37992933e090e306
Adam Zlatniczki <adam.zlatniczki@cs.bme.hu>
2018-09-05 22:34:06 +0200

    [R] added data export feature

--- C++/OpenMP/src/causality.cpp
+++ C++/OpenMP/src/causality.cpp
@@ -116,8 +116,32 @@ double* infer_causality(double* x, double* y, unsigned int n, unsigned int emb_d
 	return final_probs;
 }
 
-void infer_causality_R(double* probs_return, double* x, double* y, unsigned int* n, unsigned int* emb_dim, unsigned int* tau, unsigned int* k_range, unsigned int* len_range, double* eps/*=0.05*/, double* c/*=3.0*/, double* bins/*=20.0*/, unsigned int* downsample_rate/*=1*/){
-	double* probs = infer_causality(x, y, *n, *emb_dim, *tau, k_range, *len_range, *eps, *c, *bins, *downsample_rate);
-	for (int i=0; i<5; i++) probs_return[i] = probs[i];
+void infer_causality_R(double* probs_return, double* x, double* y, unsigned int* n, unsigned int* emb_dim, unsigned int* tau, unsigned int* k_range, unsigned int* len_range, double* eps/*=0.05*/, double* c/*=3.0*/, double* bins/*=20.0*/, unsigned int* downsample_rate/*=1*/, double* export_dims/*=NULL*/, double* export_stdevs/*=NULL*/){
+    double** exported_dims_p = NULL;
+    double** exported_stdevs_p = NULL;
+    
+    bool export_data = export_dims != NULL && export_stdevs != NULL;
+    
+    if (export_data) {
+        double* exported_dims = NULL;
+        double* exported_stdevs = NULL;
+        
+        exported_dims_p = &exported_dims;
+        exported_stdevs_p = &exported_stdevs;
+    }
+    
+	double* probs = infer_causality(x, y, *n, *emb_dim, *tau, k_range, *len_range, *eps, *c, *bins, *downsample_rate, exported_dims_p, exported_stdevs_p);
+	
+    for (int i=0; i<5; i++) probs_return[i] = probs[i];
+    
+    if (export_data) {
+        for (int i=0; i<*len_range * 4; i++) {
+            export_dims[i] = exported_dims_p[0][i];
+            export_stdevs[i] = exported_stdevs_p[0][i];
+        }
+        delete[] exported_dims_p[0];
+        delete[] exported_stdevs_p[0];
+    }
+    
 	delete[] probs;
 }
--- R/dimensionalcausality/R/wrapper.R
+++ R/dimensionalcausality/R/wrapper.R
@@ -1,24 +1,54 @@
-infer_causality <- function(x, y, emb_dim, tau, k_range, eps=0.05, c=3.0, bins=20.0, downsample_rate=1){
+infer_causality <- function(x, y, emb_dim, tau, k_range, eps=0.05, c=3.0, bins=20.0, downsample_rate=1, export_data=F, plot=T){
+    export_data = export_data | plot
 
-  n <- length(x)
-  len_range <- length(k_range)
+    n <- length(x)
+    len_range <- length(k_range)
 
-  probs <- c(0, 0, 0, 0, 0)
+    probs <- c(0, 0, 0, 0, 0)
+    dims <- NULL
+    stdevs <- NULL
+    ret <- NULL
 
-  ret <- .C("infer_causality_R",
-     probs=probs,
-     as.numeric(x),
-     as.numeric(y),
-     as.integer(n),
-     as.integer(emb_dim),
-     as.integer(tau),
-     as.integer(k_range),
-     as.integer(len_range),
-     as.numeric(eps),
-     as.numeric(c),
-     as.numeric(bins),
-     as.integer(downsample_rate)
-  )
+    if (export_data) {
+        dims <- rep(0, len_range * 4)
+        stdevs <- rep(0, len_range * 4)
 
-  return(ret$probs)
+        ret <- .C("infer_causality_R",
+            probs=probs,
+            as.numeric(x),
+            as.numeric(y),
+            as.integer(n),
+            as.integer(emb_dim),
+            as.integer(tau),
+            as.integer(k_range),
+            as.integer(len_range),
+            as.numeric(eps),
+            as.numeric(c),
+            as.numeric(bins),
+            as.integer(downsample_rate),
+            dims=dims,
+            stdevs=stdevs
+        )
+    
+        dims = matrix(ret$dims, nrow=len_range, ncol=4, byrow=T)
+        stdevs = matrix(ret$stdevs, nrow=len_range, ncol=4, byrow=T)
+    
+    } else {
+        ret <- .C("infer_causality_R",
+            probs=probs,
+            as.numeric(x),
+            as.numeric(y),
+            as.integer(n),
+            as.integer(emb_dim),
+            as.integer(tau),
+            as.integer(k_range),
+            as.integer(len_range),
+            as.numeric(eps),
+            as.numeric(c),
+            as.numeric(bins),
+            as.integer(downsample_rate)
+        )
+    }
+  
+    return(list("probs" = ret$probs, "dims" = dims, "stdevs" = stdevs))
 }
